0b9a56ebe37c9af5066b989e793a97607df74fe0
scorreah <scorreah@eafit.edu.co>
2022-05-24 20:28:24 -0500

    Eliminacion Gaussiana y Sustitución regresiva

--- python/eliminacion_gaussiana.py
+++ python/eliminacion_gaussiana.py
@@ -1,14 +1,16 @@
 def eliminacion_gausiana(A,b, n):
-  Ab=[]
-  x=[]
-  sum=0
-  #Matriz ampliada
-  for i in range(n):
-    A[i].append(b[i])
-    x.append(0)
+    print("Empezo el metodo:")
+    Ab=[]
+    x=[]
+    sum=0
+    #Matriz ampliada
+    for i in range(n):
+        A[i].append(b[i])
+        x.append(0)
     Ab = A
+    
     #Eliminacion Gausiana
-    for k in range(1, n-1): #Array de pivotes
+    for k in range(0, n): #Array de pivotes
         for i in range(k+1, n): #Hallar multiplicador, vuelve cero Ab[i][k]
             M = Ab[i][k]/Ab[k][k]
             Ab[i][k] = 0
@@ -16,13 +18,16 @@ def eliminacion_gausiana(A,b, n):
             for j in range(k+1, n+1): #Resta la filaActual - anterios*multiplicador
                 Ab[i][j] = Ab[i][j] - M*Ab[k][j]
     
-    
+    print("Empezo el sustitución:")
     #Sustitución regresiva
-    for k in range(n, 1, -1):
+    for k in range(n, 0, -1):
         sum = 0
-        for j in range(k+1, n):
-            sum += Ab[k][j]*x[j]
-        x[k-1] = (Ab[k][n+1] - sum)/Ab[k][k]
+        for j in range(k, n):
+            sum += Ab[k-1][j]*x[j]
+        x[k-1] = (Ab[k-1][n] - sum)/Ab[k-1][k-1]
+    for i in range(n):
+        print('x' + str(i) + '=' + str(x[i]))
+        
 
 A = [[-7, 2, -3, 4],
      [5, -1, 14, -1],
